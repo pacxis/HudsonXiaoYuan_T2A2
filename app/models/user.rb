e8bf91243d5ee7de9b27fa06c177f9a2909aca9c
pacxis <yuan_hudson@hotmail.com>
2022-05-23 01:01:21 +1000

    added-validations-back

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile
 
-  Include default devise modules. Others available are:
-  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
